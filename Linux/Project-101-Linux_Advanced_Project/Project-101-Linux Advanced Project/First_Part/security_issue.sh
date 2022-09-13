7ea0e3ad4c141b221d86dff91f33a72235a5ed53
Emre <51322709+SenseiRofu@users.noreply.github.com>
2022-09-13 15:10:14 +0300

    qq

--- Linux/Project-101-Linux_Advanced_Project/Project-101-Linux Advanced Project/First_Part/security_issue.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat event_history.csv | grep -i ^serdar | cut -d , -f1,10 | awk -F: '/""i/ {print $2}' | tr -d ' ",]}'| tee result.txt
--- Linux/Project-101-Linux_Advanced_Project/Project-101-Linux Advanced Project/Forth_Part/Task_2/bash.sh
+++
@@ -1,1 +0,0 @@
-cat certificate.pem | sed 's/\\n/\n/g' | tee new.pem
