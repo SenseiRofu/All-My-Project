f04b4e51d12b907c67f1c411fe337002f8c037c6
Rofu <emretokac1992@gmail.com>
2022-10-20 01:08:59 +0300

    NewTestAsgroupv2

--- Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -5,7 +5,7 @@ apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
 git clone https://$TOKEN@github.com/SenseiRofu/All-My-Project.git
-cd /home/ubuntu/All-My-Project/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/
+cd /home/ubuntu/All-My-Project/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
